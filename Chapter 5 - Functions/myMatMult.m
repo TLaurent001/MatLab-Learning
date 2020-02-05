3117db6d8d52b5802ac449274d21403623357f2a
Thomas Laurent <34180505+TLaurent001@users.noreply.github.com>
2020-02-04 20:33:34 -0800

    Added myMatMult

---
+++ Chapter 5 - Functions/myMatMult.m
@@ -0,0 +1,19 @@
+function [M] = myMatMult(P,Q)
+
+msofP = size(P);
+msofQ = size(Q);
+M = zeros(msofP(1),msofQ(2));
+
+for m = 1:(msofP(1))
+    for n = 1:msofQ(2)
+        tempvar = 0;
+        for k = 1:msofP(2)
+             tempvar = tempvar + P(m,k)*Q(k,n);
+        end
+        M(m,n) = tempvar;
+    end
+end
+
+end
+
+            
