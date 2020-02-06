88b44442adf7505ca000623ec986050c8b710ad2
Thomas Laurent <34180505+TLaurent001@users.noreply.github.com>
2020-02-05 17:39:19 -0800

    Added is_fibonacci function

---
+++ tlaurent Library/is_fibonacci.m
@@ -0,0 +1,18 @@
+function [output] = is_fibonacci(input)
+
+previousvalue = 0;
+currentvalue = 1;
+tf = false();
+
+while currentvalue < input+1
+    tempvar = previousvalue + currentvalue;
+    if (previousvalue + currentvalue) == input
+        tf = true();
+    end
+    previousvalue = currentvalue;
+    currentvalue = tempvar;
+end
+
+output = tf;
+end
+    
