fa0cd24bdf9b3efb54dfe1baa885e11569741f4c
Thomas Laurent <34180505+TLaurent001@users.noreply.github.com>
2020-02-07 10:33:09 -0800

    Made functions prettier

--- Chapter 5 - Functions/getfibonacci.m
+++ Chapter 5 - Functions/getfibonacci.m
@@ -1,13 +1,13 @@
-function [output1] = getfibonacci(input1)
+function [output] = getfibonacci(input)
 % This function gets a fibonacci number. This function is only meant to be
-% used in myNfibPrime function
+% used in myNfibPrime function.
 previousvalue1 = 0;
 currentvalue1 = 1;
 tempvar1 = 0;
-for i = 1:input1
+for i = 1:input
     tempvar1 = previousvalue1 + currentvalue1;
     previousvalue1 = currentvalue1;
     currentvalue1 = tempvar1;
 end 
-output1 = tempvar1;    
+output = tempvar1;    
 end
--- Chapter 5 - Functions/myMax.m
+++ Chapter 5 - Functions/myMax.m
@@ -1,14 +1,24 @@
-function [M] = myMax(A)
+function [X, index_value] = lowest(A)
 
 i = 1;
-M = 1;
+index = 1;
+tempvar = nan;
 
-while i < (length(A)+1)
-    if A(i) > M
-        M = A(i);
+while i < length(A) +1
+    if i == 1
+        tempvar = A(i);
+        index = i;
+    elseif A(i) < tempvar
+        tempvar = A(i);
+        index = i;
     end
     i = i +1;
 end
 
-    
+X = tempvar;
+index_value = index;
+
 end
+
+       
+        
